35650f8c43d7f9e3fb1d7e1bedaa7b13fe3edfb7
Julius Bendezu <julius.bendezu@gmail.com>
2020-12-01 20:23:57 +0100

    Problem 1

--- Day1/1.js
+++ Day1/1.js
@@ -1,3 +1,18 @@
 const { fileToArr } = require('../common.js');
 
-const inputArr = fileToArr('./input.txt')
+const input = fileToArr('./input.txt')
+
+let result;
+for (let i = 0; i < input.length; i++) {
+	for (let j = 0; j < input.length; j++) {
+		if ((2020 == Number(input[i]) + Number(input[j])) && (i != j)) {
+			result = Number(input[i]) * Number(input[j]);
+			break;
+		}
+	}
+	if (result) {
+		break;
+	}
+}
+
+console.log(result);
