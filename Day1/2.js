7901aa00f99f68112fe60616de413b63cd6404ad
Julius Bendezu <julius.bendezu@gmail.com>
2020-12-01 20:33:23 +0100

    Day 1 Problem 2

---
+++ Day1/2.js
@@ -0,0 +1,17 @@
+const func = () => {
+	const { fileToArr } = require('../common.js');
+	let input = fileToArr('./input.txt')
+	input = input.map(n => Number(n));
+
+	for (let i = 0; i < input.length; i++) {
+		for (let j = 0; j < input.length; j++) {
+			for (let k = 0; k < input.length; k++) {
+				if (2020 == input[i] + input[j] + input[k]) {
+					return input[i] * input[j] * input[k];
+				}
+			}
+		}
+	}
+}
+
+console.log(func());
