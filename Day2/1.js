109afd61aad4f33f340d642f7cde42107b003522
Julius Bendezu <julius.bendezu@gmail.com>
2020-12-02 19:09:41 +0100

    Helper functions for solving the problem for real this time

---
+++ Day2/1.js
@@ -0,0 +1,23 @@
+
+const cleanInput = (inputLine) => {
+	const parts = inputLine.split(':');
+	const password = parts[1].trim();
+	let arr = parts[0].split(' ');
+	let range = arr[0].split('-');
+	const policy = {
+		min: range[0],
+		max: range[1],
+		letter: arr[1]
+	}
+	return { policy, password };
+}
+
+const checkPassword = (policy, password) => {
+	const { min, max, letter } = policy;
+	let letterCount = 0;
+	password.split('').forEach(ch => {
+		if (ch == letter) letterCount++;
+	});
+	return min <= letterCount && letterCount <= max;
+}
+
