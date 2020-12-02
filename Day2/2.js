592867821316209b297a53ef68ace4628d014805
Julius Bendezu <julius.bendezu@gmail.com>
2020-12-02 21:00:03 +0100

    Solution day2 part 2

--- Day2/2.js
+++ Day2/2.js
@@ -1,10 +1,14 @@
 const checkPassword = (policy, password) => {
-
+	let { min: idx1, max: idx2, letter } = policy;
+	idx1 = Number(idx1) - 1;
+	idx2 = Number(idx2) - 1;
+	console.log(idx1, idx2);
+	return (password.charAt(idx1) == letter || password.charAt(idx2) == letter) && password.charAt(idx1) != password.charAt(idx2);
 }
 
-const fileToArr = require('../common');
+const { fileToArr } = require('../common');
 const input = fileToArr('./input.txt');
-const cleanInput = require('./utils');
+const { cleanInput } = require('./utils');
 
 let count = 0;
 input.forEach(line => {
