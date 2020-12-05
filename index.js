f765a6b9513f99842cc214564659c115822a9606
Julius Bendezu <julius.bendezu@gmail.com>
2020-12-05 02:42:41 +0100

    Add script for running days

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const runDay = (day) => {
+	const path = `Day${day}`;
+	process.chdir(`${__dirname}/${path}`);
+	const pt1 = require(`./${path}/1`);
+	const pt2 = require(`./${path}/2`);
+	//Add pt1 & pt2 to benchmark suite
+	console.log(`\n\n-- ${day} Solutions for day ${day} --\n\nPart 1: ${pt1()}\tPart 2: ${pt2()}`);
+}
+
+const runDays = () => {
+	require('fs')
+		.readdirSync('.')
+		.filter(dir => /Day[0-9]+/.test(dir))
+		.map(day => /([0-9]+)/.exec(day)[0])
+		.forEach(runDay);
+}
+
+const day = process.argv[2];
+if (day) {
+	runDay(day);
+} else {
+	runDays();
+}
