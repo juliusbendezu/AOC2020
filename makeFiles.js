97d2ed295b9220a59298f51d2818f72b377cf9e7
Julius Bendezu <julius.bendezu@gmail.com>
2020-12-07 11:18:35 +0100

    Add tree-data-structure dependency, will (try to) use in solution for day 2

--- makeFiles.js
+++ makeFiles.js
@@ -4,9 +4,10 @@ const today = new Date().getDate();
 const path = `./Day${today}`;
 
 try {
+	const snippet = 'const solution = () => {\nconst { fileToArr } = require(\'../utils\');\n }\n\nmodule.exports = solution;';
 	fs.mkdirSync(path);
-	fs.writeFileSync(`${path}/1.js`, '\n\nmodule.exports = solution');
-	fs.writeFileSync(`${path}/2.js`, '\n\nmodule.exports = solution');
+	fs.writeFileSync(`${path}/1.js`, snippet);
+	fs.writeFileSync(`${path}/2.js`, snippet);
 	fs.openSync(`${path}/input.txt`, 'w');
 	fs.openSync(`${path}/problem.txt`, 'w');
 } catch (err) {
