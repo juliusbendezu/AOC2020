ff55a9b609978398b1d77e6c77145c960473c11f
Julius Bendezu <julius.bendezu@gmail.com>
2020-12-07 11:17:42 +0100

    Solution day7 part 1

---
+++ Day7/1.js
@@ -0,0 +1,37 @@
+
+const bagContainingBag = (rule, bag = 'shiny gold') => {
+	const regex = new RegExp(bag + ' bags*(\\.|\\,)');
+	if (regex.test(rule)) {
+		console.log(rule);
+		return rule.split(' ').slice(0, 2).join(' ');
+	}
+	return null;
+}
+const solution = () => {
+	const { fileToArr } = require('../utils');
+	const rules = fileToArr('./input.txt');
+
+	const eventuallyContainsGold = ['shiny gold'];
+	const checkedBags = [];
+	const result = new Set();
+
+	while (!!eventuallyContainsGold.length) {
+		const bagToCheck = eventuallyContainsGold.shift();
+		if (checkedBags.includes(bagToCheck)) {
+			continue;
+		}
+
+		for (const rule of rules) {
+			const bag = bagContainingBag(rule, bagToCheck);
+			if (bag) {
+				eventuallyContainsGold.push(bag);
+				result.add(bag);
+			}
+		}
+		checkedBags.push(bagToCheck);
+	}
+
+	return result.size;
+}
+
+module.exports = solution;
