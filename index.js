9b77036d4c828258ce31a867cf661c1529477850
Julius Bendezu <julius.bendezu@gmail.com>
2020-12-08 01:22:46 +0100

    Add script to run solutions with benchmarking

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 
-const Benchmark = require('benchmark')
-
-const runDay = (day) => {
+const runDay = (day, benchmarking) => {
 	const path = `Day${day}`;
 	process.chdir(`${__dirname}/${path}`);
 	const pt1 = require(`./${path}/1`);
 	const pt2 = require(`./${path}/2`);
-	//Add pt1 & pt2 to benchmark suite
-	console.log(`\n\n-- ${day} Solutions for day ${day} --\n\nPart 1: ${pt1()}\tPart 2: ${pt2()}`);
+
+	if (benchmarking) {
+		const Benchmark = require('benchmark');
+		const suite = new Benchmark.Suite;
+		suite.add(`Day ${day} part 1`, pt1)
+			.add(`Day ${day} part 2`, pt2)
+			.on('cycle', (event) => console.log(String(event.target)))
+			.run();
+	} else {
+		console.log(`\n\n-- ${day} Solutions for day ${day} --\n\nPart 1: ${pt1()}\tPart 2: ${pt2()}`);
+	}
 }
 
-const runDays = () => {
+const runDays = (benchmarking) => {
 	require('fs')
 		.readdirSync('.')
 		.filter(dir => /Day[0-9]+/.test(dir))
 		.map(day => /([0-9]+)/.exec(day)[0])
-		.forEach(runDay);
+		.forEach(day => runDay(day, benchmarking));
 }
 
-const day = process.argv[2];
+const args = process.argv.slice(2);
+const benchmarking = 'true' == args[0];
+console.log('benchmarking: ', benchmarking);
+const day = args[1];
 if (day) {
-	runDay(day);
+	runDay(day, benchmarking);
 } else {
-	runDays();
+	runDays(benchmarking);
 }
