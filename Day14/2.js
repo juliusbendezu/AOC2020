ff00e471b6583ff52ec5905a20f697420456cd3a
Julius Bendezu <julius.bendezu@gmail.com>
2020-12-18 00:04:27 +0100

    Solution day 14 part 2

--- Day14/2.js
+++ Day14/2.js
@@ -1,6 +1,18 @@
 const solution = () => {
-	const { fileToArr } = require('../utils');
+	const { parseInput, applyMaskFloating } = require('./utils');
+	const input = parseInput('./input.txt', 36);
 
+	const adresses = new Map();
+	for (const group of input) {
+		let mask = group[0];
+		let actions = group.slice(1);
+		actions.forEach(action => {
+			const [adress, value] = action;
+			const newAdresses = applyMaskFloating(mask, Number(adress));
+			newAdresses.forEach(a => adresses.set(a, Number(value)));
+		});
+	}
+	return Array.from(adresses.values()).reduce((acc, curr) => acc + curr, 0);
 }
 
 module.exports = solution;
--- Day14/utils.js
+++ Day14/utils.js
@@ -1,3 +1,6 @@
+
+const _ = require('lodash');
+
 const parseInput = (file, maskLength) => {
 	const { fileToArr } = require('../utils');
 	let inputGroups = fileToArr(file, 'mask = ').map(line => line.trim().split('\n')).slice(1);
@@ -31,4 +34,50 @@ const applyMask = (mask, value) => {
 	return Number.parseInt(value.join(''), 2);
 }
 
-module.exports = { parseInput, applyMask };
+const applyMaskFloating = (mask, value) => {
+	let floatingCount = 0;
+
+	value = value.toString(2).split(''); //Make binary then to char array
+	if (value.length < mask.length) {
+		while (value.length < mask.length) {
+			value.unshift(0);
+		}
+	}
+	for (let i = 0; i < mask.length; i++) {
+		if ('X' == mask.charAt(i)) {
+			floatingCount++;
+		}
+		if (0 != mask.charAt(i)) {
+			value[i] = mask.charAt(i);
+		}
+	}
+
+	return floatingCount > 0 ? getValuesFromFloating(value, floatingCount) : [Number.parseInt(value.join(''), 2)];
+}
+
+const getValuesFromFloating = (value, floatingCount) => {
+	let values = [];
+	const maxBits = Math.pow(2, floatingCount) - 1;
+
+	//X:es can fit binary numbers to create all possible combos, numbers depend on floatingCount/x-count
+	//Map every X-bit to a bit in the binary number, for every number, to create all combos
+	for (let bit = 0; bit <= maxBits; bit++) {
+		let bin = bit.toString(2);
+		bin = _.padStart(bin, value.length, '0');
+
+		const newValue = [];
+		let binIdx = bin.length - 1;
+		for (let i = 0; i < value.length; i++) {
+			if ('X' == value[i]) {
+				newValue.push(bin[binIdx]);
+				binIdx--;
+			} else {
+				newValue.push(value[i]);
+			}
+		}
+		values.push(newValue);
+	}
+	return values.map(v => Number.parseInt(v.join(''), 2));
+}
+
+module.exports = { parseInput, applyMask, applyMaskFloating };
